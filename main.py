b5f23a84089446760a0027b6621b86e964f24f36
Michael Piotrowski <mlp95747@gmail.com>
2022-07-22 12:55:33 -0700

    Simple Python Chat-Client

---
+++ main.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+
+
+HOST = '192.168.182.1'
+PORT = 9090
+
+server = server.socket(server.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+# Broadcast Function
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+# Receive Function
+
+def receive():
+    while true:
+        client, address = server.accept()
+        print(f"connected with {str(address)}!")
+
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(clients)
+
+# Handle Function
+
